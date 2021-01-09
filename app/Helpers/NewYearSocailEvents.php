64978aa5bc95fd6e3ea95c7dc68a126fe79411b0
abdallah <30749992+abdalla93@users.noreply.github.com>
2021-01-09 15:18:17 +0200

    over next 12 months instead of 12 months of this year

--- app/Helpers/NewYearSocailEvents.php
+++ app/Helpers/NewYearSocailEvents.php
@@ -7,35 +7,46 @@ use Carbon\Carbon;
 class NewYearSocailEvents
 {
     protected $months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'];
-    protected $headers=['Month', 'Brunch & Catchup', 'Thirsty Thursday', 'Friday Fry-up'];
-    protected $fileName='socail-events';
+    protected $headers = ['Month', 'Brunch & Catchup', 'Thirsty Thursday', 'Friday Fry-up'];
+    protected $fileName = 'socail-events';
 
     public function makeSocailEvents()
     {
         //array of each month object data
         $socailEvents = [];
 
-        foreach ($this->months as $monthIndex => $month) {
-            $thisMonthIndex = $monthIndex + 1;
-            $MonthlySocalEvents=[];
+        $thisMonth=Carbon::parse()->month;
+        $thisYear = Carbon::parse()->year;
+        
+        $month = $thisMonth;
+        do {
+            $MonthlySocalEvents = [];
 
-            array_push($MonthlySocalEvents,$month);
+            $monthName = $this->months[$month - 1];
             //Month Date
-            $date = $thisMonthIndex . '/' . Carbon::now()->year;
-            array_push($MonthlySocalEvents,$date);
-            //First Monday of the month
-            $brunchAndCatchup = Carbon::parse('first monday of' . $month)->format('d/m/Y');
-            array_push($MonthlySocalEvents,$brunchAndCatchup);
+            $date = $month . '/' . $thisYear;
+            array_push($MonthlySocalEvents, $date);
+            //First Monday of the month 
+            $brunchAndCatchup = Carbon::parse('first monday of' . $monthName . ' ' . $thisYear)->format('d/m/Y');
+            array_push($MonthlySocalEvents, $brunchAndCatchup);
             //3rd Thursday of the month
-            $thirstyThursday = Carbon::parse('third thursday of' . $month)->format('d/m/Y');
-            array_push($MonthlySocalEvents,$thirstyThursday);
+            $thirstyThursday = Carbon::parse('third thursday of' . $monthName . ' ' . $thisYear)->format('d/m/Y');
+            array_push($MonthlySocalEvents, $thirstyThursday);
             //last friday of the month
-            $fridayFryUp = Carbon::parse('last friday of' . $month)->format('d/m/Y');
-            array_push($MonthlySocalEvents,$fridayFryUp);
+            $fridayFryUp = Carbon::parse('last friday of' . $monthName . ' ' . $thisYear)->format('d/m/Y');
+            array_push($MonthlySocalEvents, $fridayFryUp);
 
             array_push($socailEvents, $MonthlySocalEvents);
-        }
-        $generateCSV=new GenerateCSV();
+
+            if ($month == 12) {
+                $month = 1;
+                $thisYear = Carbon::now()->addYear()->year;
+            } else {
+                $month++;
+            }
+
+        } while ($month != $thisMonth);
+        $generateCSV = new GenerateCSV();
 
         return $generateCSV->GenerateFileCSV($this->fileName,$this->headers,$socailEvents);
     }
